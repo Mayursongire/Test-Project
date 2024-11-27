a208687ad4acf069b402cda5c611ec4cfef759e5
MayurSongire19 <142386876+Mayursongire@users.noreply.github.com>
2024-11-27 11:41:07 +0530

    Services added

---
+++ Test_Project-master (1)/Test_Project-master/Test_Project/Services/CategoryService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Test_Project.EF;
+using Test_Project.Models;
+
+namespace Test_Project.Services
+{
+    public class CategoryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Constructor to inject ApplicationDbContext
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Add a new category
+        public void AddCategory(Category category)
+        {
+            // Check if category already exists
+            if (_context.Category.Any(c => c.CategoryName == category.CategoryName))
+            {
+                throw new Exception("Category already exists.");
+            }
+
+            // Add the category to database
+            _context.Category.Add(category);
+            _context.SaveChanges();
+        }
+
+        // Update an existing category
+        public void UpdateCategory(Category category)
+        {
+            var existingCategory = _context.Category.SingleOrDefault(c => c.CategoryId == category.CategoryId);
+            if (existingCategory == null)
+            {
+                throw new Exception("Category not found.");
+            }
+
+            existingCategory.CategoryName = category.CategoryName;
+            _context.SaveChanges();
+        }
+
+        // Get all categories
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _context.Category.ToList();
+        }
+
+        // Get a category by ID
+        public Category GetCategoryById(int id)
+        {
+            return _context.Category.SingleOrDefault(c => c.CategoryId == id);
+        }
+
+        // Delete a category
+        public void DeleteCategory(int id)
+        {
+            var category = _context.Category.SingleOrDefault(c => c.CategoryId == id);
+            if (category != null)
+            {
+                _context.Category.Remove(category);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Test_Project-master (1)/Test_Project-master/Test_Project/Services/ProductService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Test_Project.EF;
+using Test_Project.Models;
+
+namespace Test_Project.Services
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Constructor to inject ApplicationDbContext
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Add new product to the database
+        public void AddProduct(Product product)
+        {
+            // Business logic: Check if the product name is already taken
+            if (_context.Product.Any(p => p.ProductName == product.ProductName))
+            {
+                throw new Exception("Product name already exists.");
+            }
+
+            // Add product
+            _context.Product.Add(product);
+            _context.SaveChanges();
+        }
+
+        // Update existing product
+        public void UpdateProduct(Product product)
+        {
+            var existingProduct = _context.Product.SingleOrDefault(p => p.ProductId == product.ProductId);
+            if (existingProduct == null)
+            {
+                throw new Exception("Product not found.");
+            }
+
+            existingProduct.ProductName = product.ProductName;
+            existingProduct.ProductCategoryId = product.ProductCategoryId;
+            _context.SaveChanges();
+        }
+
+        // Get all products
+        public IEnumerable<ProductCategoryViewModel> GetAllProducts()
+        {
+            var data = (from p in _context.Product
+                        join c in _context.Category on p.ProductCategoryId equals c.CategoryId
+                        select new ProductCategoryViewModel
+                        {
+                            ProductId = p.ProductId,
+                            ProductName = p.ProductName,
+                            CategoryId = c.CategoryId,
+                            CategoryName = c.CategoryName
+                        }).ToList();
+
+            return data;
+        }
+
+        // Get a single product by ID
+        public Product GetProductById(int id)
+        {
+            return _context.Product.SingleOrDefault(p => p.ProductId == id);
+        }
+
+        // Delete a product
+        public void DeleteProduct(int id)
+        {
+            var product = _context.Product.SingleOrDefault(p => p.ProductId == id);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
