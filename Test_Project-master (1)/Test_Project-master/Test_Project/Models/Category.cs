c0c0c0dc1a60ba56453f5db48f98bfe8828663b5
MayurSongire19 <142386876+Mayursongire@users.noreply.github.com>
2024-11-27 11:14:39 +0530

    Update Category.cs

--- Test_Project-master (1)/Test_Project-master/Test_Project/Models/Category.cs
+++ Test_Project-master (1)/Test_Project-master/Test_Project/Models/Category.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
-namespace Test_Project.Models
+public class Category
 {
-    public class Category
-    {
-        [Key]
-        [Display(Name ="Category ID")]
-        public int CategoryId { get; set; }
+    [Key]
+    [Display(Name ="Category ID")]
+    public int CategoryId { get; set; }
 
-        [Required]
-        [MaxLength(500)]
-        [Display(Name ="Category Name")]
-        public string CategoryName { get; set; }
-    }
+    [Required]
+    [MaxLength(500)]
+    [Display(Name ="Category Name")]
+    public string CategoryName { get; set; }
+
+    [Display(Name = "Is Active")]
+    public bool IsActive { get; set; }  
 }
